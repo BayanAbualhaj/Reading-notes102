1e505bf63e10dbb563179d80284258732a1a69c5
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2020-12-15 22:53:04 +0200

    lab6

---
+++ lap05.js
@@ -0,0 +1,33 @@
+ver Number = 11;
+ver month = "October";
+ver OK = true;
+ver NO = false; 
+
+// its about my birthday! 
+
+if (number == 11){
+    alert(LUCKY DAY!);
+} else {
+    alert(NOT a lucky day!);
+}
+
+if (number < 11){
+    alert (Almost Here!);
+} else {
+    alert (Till Next Year!);
+}
+
+if (month == october) {
+    alert (MY MONTH);
+} else {
+    alert(NOT MY MONTH);
+}
+
+if (OK){
+    alert("OKAY")
+}
+
+if (no){
+    alert ("NO")
+}
+
